9cbf824a636a89f993f02ed192c2b300d459dffc
Augustin Cavalier <waddlesplash@gmail.com>
2017-02-02 11:07:45 -0500

    index.php: Add link to the exported User Guide.
    Fixes https://dev.haiku-os.org/ticket/13265.

--- userguide/index.php
+++ userguide/index.php
@@ -10,11 +10,13 @@ if ($user_logged_in)
 ?>
 <h1>Haiku User Guide Translations</h1>
 <p>If you're looking for the actual documentation for Haiku, please visit the pages of the
-<a href="http://www.haiku-os.org/docs/userguide/en/contents.html">Official Haiku User Guide</a>
+<a href="https://www.haiku-os.org/docs/userguide/en/contents.html">Official Haiku User Guide</a>
 of the Haiku project.</p>
 <p>If you would like to help with translation, visit our
-<a href="http://dev.haiku-os.org/wiki/i18n">Wiki</a> and learn more on how to
-<a href="http://dev.haiku-os.org/wiki/i18n/UserGuide">get involved</a>.</p>
+<a href="https://dev.haiku-os.org/wiki/i18n">Wiki</a> and learn more on how to
+<a href="https://dev.haiku-os.org/wiki/i18n/UserGuide">get involved</a>.</p>
+<p>Or you can also browse the
+<a href="export/docs/userguide/en/contents.html">work-in-progress version of the User Guide</a> here.</p>
 <br/>
 <form action="documents.php?login" method="post">
 <dl class="fieldset">
