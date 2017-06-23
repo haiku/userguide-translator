5dc7038f3d2a3de0b91d1095219756c2f44b189b
Augustin Cavalier <waddlesplash@gmail.com>
2017-06-23 18:34:35 -0400

    documents -> documents_old; documents2 -> documents.

--- userguide/documents.php
+++ userguide/documents.php
@@ -31,89 +31,129 @@ $ltop .= ($ltop ? ' • ' : '') .'<a href="log.php">Changelog</a>';
 
 include('inc/start_html.php');
 
-$first_row = '<th>English (original)</th>';
-$lang_rows = array();
-$lang_codes = array();
+$sel_lang = (isset($_GET['l']) ? validate_lang($_GET['l']) :  '');
 
+echo "<h1>Translation status</h1>\nDisplay: ";
+if ($sel_lang)
+	echo '<a href="documents.php">';
+echo 'Every language';
+if ($sel_lang)
+	echo '</a>';
+
+$sel_lang_name = '';
+$sel_loc_name = '';
 $req = db_query('SELECT * FROM ' . DB_LANGS . ' ORDER BY lang_name');
+$language_names = array();
 while ($row = db_fetch($req)) {
-	$first_row .= '<th>' . htmlspecialchars($row['lang_name']) . '</th>';
-	$lang_codes[] = $row['lang_code'];
+	$lang_name = htmlspecialchars($row['lang_name']);
+	$lang_code = $row['lang_code'];
+	if ($lang_code == $sel_lang) {
+		$sel_lang_name = $lang_name;
+		$sel_loc_name = htmlspecialchars($row['loc_name']);
+		echo ' • ' . $lang_name;
+	} else {
+		echo ' • <a href="documents.php?l=' . $lang_code . '">' . $lang_name . '</a>';
+	}
+	$language_names[$lang_code] = $lang_name;
 }
-$len = count($lang_codes) + 1;
-db_free($req);
 
+if (!$sel_lang_name) // We haven't found the specified language code
+	$sel_lang = '';
+
+if ($sel_lang) { // Specific status page
 ?>
-
-<h1>Current translation status</h1>
-<table class="list compact">
-<thead>
-<tr>
-<?=$first_row?>
-</tr>
-</thead>
+<br/>
+<table class="list">
+<thead><tr>
+<th>Document</th><th><?=$sel_lang_name?> (<?=$sel_loc_name?>)</th>
+</tr></thead>
 <tbody>
 <?php
-$req = db_query('SELECT * FROM ' . DB_DOCS . ' ORDER BY name ASC');
+	$req = db_query('
+		SELECT doc_id, name, path_original, path_translations, strings_count, ' . "
+			count_${sel_lang}, count_fuzzy_${sel_lang} FROM " . DB_DOCS . '
+		ORDER BY name ASC');
+	while ($row = db_fetch($req)) {
+		$doc_id = $row['doc_id'];
 
-while ($row = db_fetch($req)) {
-	$doc_id = $row['doc_id'];
+		$path_trans = 'view.php?doc_id=' . $doc_id . '&amp;l=' . $sel_lang;
+		$name = $row['name'] ? $row['name'] : $row['path_original'];
+		$name = htmlspecialchars($name);
+		$translate_path = 'translate.php?doc_id=' . $doc_id . '&amp;l=' . $sel_lang;
 
-	$path = 'view.php?doc_id=' . $doc_id;
-	$path_edit = 'edit.php?doc_id=' . $doc_id;
-	$block_edit = 'block_edit.php?doc_id=' . $doc_id;
-	$name = $row['name'] ? $row['name'] : $row['path_original'];
-	$name = htmlspecialchars($name);
+		$count = intval($row['strings_count']);
+		$percent = round(100 * intval($row['count_' . $sel_lang]) / $count, 0);
+		$fuzzy_count = intval($row['count_fuzzy_' . $sel_lang]);
+		$status = '<b style="color:' . color($percent, $fuzzy_count) . '">' . $percent . '%';
+		if ($fuzzy_count)
+			$status .= " — $fuzzy_count fuzzy";
+		$status .= '</b>';
 ?>
 <tr>
-<td><a href="<?=$path?>"><?=$name?></a><br/>
-<a href="<?=$block_edit?>">Block Edit</a> •
-<a href="<?=$path_edit?>">Full Edit</a></td>
-<?php
-	if ($row['strings_count'] > 0) {
-		foreach ($lang_codes as $lang_id => $lang_code) {
-			$path_trans = $row['path_translations'];
-			$path_translated = str_replace('{LANG}', $lang_code, $path_trans);
-
-			$percent = 100 * intval($row['count_' . $lang_code]);
-			$percent = round($percent / intval($row['strings_count']), 0);
-
-			$current = 'view.php?doc_id=' . $doc_id . '&amp;l=' . $lang_code;
-			$translate = 'translate.php?doc_id=' . $doc_id . '&amp;l=' . $lang_code;
-
-			$fuzzy = '';
-			if ($row['count_fuzzy_' . $lang_code])
-				$fuzzy = ' - ' . $row['count_fuzzy_' . $lang_code] . ' fuzzy';
-?>
-<td>
-<a href="<?=$current?>" title="View latest version">View</a> •
-<a href="<?=$translate?>" title="Translate the document">Translate</a><br/>
-(<b style="color:<?=color($percent, $row['count_fuzzy_' . $lang_code])?>"><?=$percent?> %<?=$fuzzy?></b>)
+<td><?=$name?></td>
+<td><a href="<?=$path_trans?>">View</a>
+<?php if ($count) { ?>• <a href="<?=$translate_path?>">Translate</a> (Progress: <?=$status?>) <?php } ?>
 </td>
-<?php
-		}
-	} else {
-?>
-<td colspan="<?=$len?>">No translatable text found in this document.</td>
-<?php
-	}
-?>
 </tr>
 <?php
-}
+	}
+	echo '</tbody></table>';
+} else { // Multilanguage status page
 ?>
-</tbody>
-</table>
-<?php
+<br/>
+<table class="list">
+<tr>
+<th>English (original)</th><?php
+	$sql = 'SELECT doc_id, name, strings_count, path_translations';
+	$count_langs = count($language_names);
+	foreach($language_names as $code => $name) {
+		$sql .= ", count_$code, count_fuzzy_$code";
+		echo "<th>$name</th>";
+	}
+	echo "\n</tr>\n";
+	$sql .= ' FROM ' . DB_DOCS . ' ORDER BY name ASC';
 
+	$req = db_query($sql);
+	while ($row = db_fetch($req)) {
+		$doc_id = $row['doc_id'];
+		$name = htmlspecialchars($row['name']);
+		$path = 'view.php?doc_id=' . $doc_id;
+		$path_edit = 'edit.php?doc_id=' . $doc_id;
+		$block_edit = 'block_edit.php?doc_id=' . $doc_id;
+		$count = intval($row['strings_count']);
+		$columns = '';
+		if ($count) {
+			foreach ($language_names as $code => $unused) {
+				$percent = round(100 * intval($row['count_' . $code]) / $count, 0);
+				$fuzzy_count = intval($row['count_fuzzy_' . $code]);
+				$columns .= '<td><a href="view.php?doc_id=' . $doc_id .
+					'&amp;l=' . $code. '" style="color:' . color($percent, $fuzzy_count) . '">' .
+					$percent . '%';
+				if ($fuzzy_count)
+					$columns .= " ($fuzzy_count f.)";
+				$columns .= '</a></td>';
+			}
+		} else {
+			$columns = '<td colspan="' . $count_langs . '">This document is not translatable.</td>';
+		}
+?>
+<tr>
+<td><a href="view.php?doc_id=<?=$doc_id?>"><?=$name?></a><br>
+<a class="c" href="<?=$block_edit?>">Block Edit</a> •
+<a class="c" href="<?=$path_edit?>">Full Edit</a></td></td><?=$columns?>
+</tr>
+<?php
+	}
+	echo "</table>\n";
+}
 include('inc/end_html.php');
 
 function color($percent, $fuzzy_count) {
-	if ($fuzzy_count > 0)
-		return 'orange';
-
 	if ($percent == 100)
 		return 'green';
+
+	if ($fuzzy_count > 0)
+		return 'orange';
 
 	if ($percent == 0)
 		return 'black';
